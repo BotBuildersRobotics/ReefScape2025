50c51051394ea5d38111beeb615d35491abc6adb
Dudeofawsome24 <ryansanierosales@gmail.com>
2025-01-13 19:04:46 +1000

    add-pivotSubsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,11 +8,15 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -20,82 +24,94 @@ public final class Constants {
     public static final int kDriverControllerPort = 0;
   }
 
-  public static final class IntakeConstants {
-		
-		
-        //common config for all intake motors
-        //its ok to have a specific motor config 
-        //but always define values here to make it easy to find.
-        public static TalonFXConfiguration IntakeFXConfig() {
-          TalonFXConfiguration config = new TalonFXConfiguration();
-
-
-          config.CurrentLimits.SupplyCurrentLimit = 20.0;
-          config.CurrentLimits.SupplyCurrentLimitEnable = true;
-         
-          config.CurrentLimits.StatorCurrentLimitEnable = true;
-          config.CurrentLimits.StatorCurrentLimit = 80;
-
-          config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-          return config;
-		  }
-	}
-
-  public static final class ElevatorConstants {
-
-    public static TalonFXConfiguration ElevatorFXConfig(){
-      TalonFXConfiguration config = new TalonFXConfiguration();
-
-      //copy the configurations because it doesn't actually exist
-      config.CurrentLimits.SupplyCurrentLimit = 20.0;
-      config.CurrentLimits.SupplyCurrentLimitEnable = true;
-     
-      config.CurrentLimits.StatorCurrentLimitEnable = true;
-      config.CurrentLimits.StatorCurrentLimit = 80;
-
-      config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-      return config;
-
-  }
-  }
+  // Right now each motor is using common config for all intake motors
+  // its ok to have a specific motor config
+  // but always define values here to make it easy to find.
 
   public static final class ClimbConstants {
-		
-		
-    //copying more constants. possible to add more constants for different motors by adding more functions but for now just use the same ones
+
+    // copying more constants. possible to add more constants for different motors
+    // by adding more functions but for now just use the same ones
     public static TalonFXConfiguration ClimbFXConfig() {
       TalonFXConfiguration config = new TalonFXConfiguration();
 
-
       config.CurrentLimits.SupplyCurrentLimit = 20.0;
       config.CurrentLimits.SupplyCurrentLimitEnable = true;
-     
+
       config.CurrentLimits.StatorCurrentLimitEnable = true;
       config.CurrentLimits.StatorCurrentLimit = 80;
 
       config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
       return config;
+    }
+
+  }
+  
+  public static final class ElevatorConstants {
+
+    public static TalonFXConfiguration ElevatorFXConfig() {
+      TalonFXConfiguration config = new TalonFXConfiguration();
+
+      // copy the configurations because it doesn't actually exist
+      config.CurrentLimits.SupplyCurrentLimit = 20.0;
+      config.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+      config.CurrentLimits.StatorCurrentLimitEnable = true;
+      config.CurrentLimits.StatorCurrentLimit = 80;
+
+      config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+      return config;
+
+    }
   }
 
+  public static final class EndEffectorConstants {
 
-}
+    // copying more constans
+    public static TalonFXConfiguration EndEffectorFXConfig() {
+      TalonFXConfiguration config = new TalonFXConfiguration();
 
-public static final class EndEffectorConstants {
-		
-		
-  //copying more constans
-  public static TalonFXConfiguration EndEffectorFXConfig() {
-    TalonFXConfiguration config = new TalonFXConfiguration();
+      config.CurrentLimits.SupplyCurrentLimit = 20.0;
+      config.CurrentLimits.SupplyCurrentLimitEnable = true;
 
+      config.CurrentLimits.StatorCurrentLimitEnable = true;
+      config.CurrentLimits.StatorCurrentLimit = 80;
 
-    config.CurrentLimits.SupplyCurrentLimit = 20.0;
-    config.CurrentLimits.SupplyCurrentLimitEnable = true;
-   
-    config.CurrentLimits.StatorCurrentLimitEnable = true;
-    config.CurrentLimits.StatorCurrentLimit = 80;
+      config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+      return config;
+    }
+  }
 
-    config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-    return config;
-}
+  public static final class IntakeConstants {
 
+    public static TalonFXConfiguration IntakeFXConfig() {
+      TalonFXConfiguration config = new TalonFXConfiguration();
+
+      config.CurrentLimits.SupplyCurrentLimit = 20.0;
+      config.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+      config.CurrentLimits.StatorCurrentLimitEnable = true;
+      config.CurrentLimits.StatorCurrentLimit = 80;
+
+      config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+      return config;
+    }
+  }
+
+  public static final class PivotConstants {
+
+    // copying more constans
+    public static TalonFXConfiguration PivotFXConfig() {
+      TalonFXConfiguration config = new TalonFXConfiguration();
+
+      config.CurrentLimits.SupplyCurrentLimit = 20.0;
+      config.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+      config.CurrentLimits.StatorCurrentLimitEnable = true;
+      config.CurrentLimits.StatorCurrentLimit = 80;
+
+      config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+      return config;
+    }
+  }
 }
--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -5,20 +5,26 @@ import frc.robot.lib.CanDeviceId;
 public class Ports {
 
     //each CAN device will have a name and a unique ID.
-    //Intake Subsystem Motors
-    public static final CanDeviceId INTAKE_TOP_ROLLER = new CanDeviceId(1, "canivore");
-
-    //Elevator Subsystem Motors
-    //Let's just say we put a lot of krakens on the robot. This is the 10002nd.
-    public static final CanDeviceId ELEVATOR = new CanDeviceId(10002, "canivore");
+    //Right now each one has a random ID
 
     //Climb Subsystem Motors
-    //more random IDs
     public static final CanDeviceId CLIMB_ONE = new CanDeviceId(9, "canivore");
     public static final CanDeviceId CLIMB_TWO = new CanDeviceId(10, "canivore");
 
+    //Elevator Subsystem Motors
+    public static final CanDeviceId ELEVATOR = new CanDeviceId(10002, "canivore");
+
     //End Effector Subsystem Motors
     public static final CanDeviceId END_EFFECTOR_ROLLER = new CanDeviceId(100, "canivore");
+
+    //Intake Subsystem Motors
+    public static final CanDeviceId INTAKE_TOP_ROLLER = new CanDeviceId(1, "canivore");
+    
+    //Pivot Subsystem Motors
+    public static final CanDeviceId PIVOT_LEFT = new CanDeviceId(139042435,"canivore");
+    public static final CanDeviceId PIVOT_RIGHT = new CanDeviceId(8972365, "canivore");
+
+    //Transfer Subsytem Motors
 
     public static final CanDeviceId LEDS = new CanDeviceId(21, "canivore");
 
