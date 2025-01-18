691e267f025040c6b3db02f56790df4be3d81f7f
BotBuilders <161176226+BotBuildersRobotics@users.noreply.github.com>
2025-01-18 17:21:39 +1000

    Updated kraken and cancoder IDs

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -36,5 +36,18 @@ public class Ports {
 	public static final int INTAKE_BEAMBREAKTWO = 2;
 
     // Wheel krakens
-    public static final CanDeviceId TOPLEFT_LEFT = new CanDeviceId(1, "canivore");
+    public static final CanDeviceId TOPRIGHT_ROT = new CanDeviceId(1, "canivore");
+    public static final CanDeviceId TOPRIGHT_DRIVE = new CanDeviceId(3, "canivore");
+    public static final CanDeviceId BOTTOMRIGHT_ROT = new CanDeviceId(4, "canivore");
+    public static final CanDeviceId BOTTOMRIGHT_DRIVE = new CanDeviceId(6, "canivore");
+    public static final CanDeviceId BOTTOMLEFT_ROT = new CanDeviceId(7, "canivore");
+    public static final CanDeviceId BOTTOMLEFT_DRIVE = new CanDeviceId(9, "canivore");
+    public static final CanDeviceId TOPLEFT_ROT = new CanDeviceId(10, "canivore");
+    public static final CanDeviceId TOPLEFT_DRIVE = new CanDeviceId(12, "canivore");
+
+    // Wheel cancoder
+    public static final CanDeviceId TOPRIGHT_ENCODER = new CanDeviceId(2, "canivore");
+    public static final CanDeviceId BOTTOMRIGHT_ENCODER = new CanDeviceId(5, "canivore");
+    public static final CanDeviceId BOTTOMLEFT_ENCODER = new CanDeviceId(8, "canivore");
+    public static final CanDeviceId TOPLEFT_ENCODER = new CanDeviceId(11, "canivore");
 }
