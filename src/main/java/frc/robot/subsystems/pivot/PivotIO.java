07f0e7f2ae547b39a2a9ebdea3dd8ad74ac05927
Daniel P <daniel@learnweaver.com>
2025-02-11 19:21:56 +1100

    Fixed PivotPosition

--- src/main/java/frc/robot/subsystems/pivot/PivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -27,6 +27,9 @@ public interface PivotIO {
         public double pivotRightCurrent = 0.0;
         public double pivotRightPosition = 0.0;
         public double pivotRightMotorPos = 0.0;
+
+
+        public double pivotPosition = 0.0;
     }
 
     
