750cf153c24bfb56a77819d061fe68afea52a0d2
BotBuilders <161176226+BotBuildersRobotics@users.noreply.github.com>
2025-02-28 15:35:33 +1000

    System Integration

--- src/main/java/frc/robot/subsystems/SuperSystem.java
+++ src/main/java/frc/robot/subsystems/SuperSystem.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import java.util.Map;
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
